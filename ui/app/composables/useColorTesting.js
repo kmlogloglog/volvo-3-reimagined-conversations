9e372c31bb261b060b138b35ea40de56087d553f
lars-liin-vml <lars.liin@vml.com>
2026-02-06 09:59:33 +0100

    Remove green color option from audio capture color palette

--- ui/app/composables/useColorTesting.js
+++ ui/app/composables/useColorTesting.js
@@ -15,7 +15,6 @@ export function useColorTesting() {
     const colorPalette = [
         { light: { r: 191, g: 154, b: 103 }, dark: { r: 191, g: 154, b: 103 } }, // Original gold
         { light: { r: 255, g: 100, b: 100 }, dark: { r: 200, g: 80, b: 80 } },   // Red
-        { light: { r: 100, g: 255, b: 100 }, dark: { r: 80, g: 200, b: 80 } },   // Green
         { light: { r: 100, g: 100, b: 255 }, dark: { r: 80, g: 80, b: 200 } },   // Blue
         { light: { r: 255, g: 255, b: 100 }, dark: { r: 200, g: 200, b: 80 } },  // Yellow
         { light: { r: 255, g: 100, b: 255 }, dark: { r: 200, g: 80, b: 200 } },  // Magenta
